e5881f8519fa6dd3d6b73260d98677d8e6f63a59
khanh <khanh.nguyen.contact@gmail.com>
2026-05-01 19:24:01 +0800

    auto_commit

--- prop_logic_kernel/types.py
+++ prop_logic_kernel/types.py
@@ -27,17 +27,47 @@ class Imp(Type):
 class Fals(Type):
     ...
 
-def to_string(t: Type) -> str:
+def _type_priority(t: Type | None) -> int:
+    if t is None:
+        return 999
+    # Imp Or And Not
+    if isinstance(t, Term):
+        return 0
+    elif isinstance(t, Not):
+        return 1
+    elif isinstance(t, And):
+        return 2
+    elif isinstance(t, Or):
+        return 3
+    elif isinstance(t, Imp):
+        return 4
+    elif isinstance(t, Fals):
+        return 0
+    else:
+        raise NotImplementedError
+
+def to_string(t: Type, parent: Type | None = None) -> str:
+    # order
+    # Imp Or And Not
     if isinstance(t, Term):
         return t.name
     elif isinstance(t, Not):
-        return f"¬{to_string(t)}"
+        return f"¬{to_string(t.this, parent=t)}"
     elif isinstance(t, And):
-        return f"({to_string(t.left)} ∧ {to_string(t.right)})"
+        s = f"{to_string(t.left, parent=t)} ∧ {to_string(t.right, parent=t)}"
+        if _type_priority(parent) <= _type_priority(t):
+            s = f"({s})"
+        return s
     elif isinstance(t, Or):
-        return f"({to_string(t.left)} ∨ {to_string(t.right)})"
+        s = f"{to_string(t.left, parent=t)} ∨ {to_string(t.right, parent=t)}"
+        if _type_priority(parent) <= _type_priority(t):
+            s = f"({s})"
+        return s
     elif isinstance(t, Imp):
-        return f"({to_string(t.left)} → {to_string(t.right)})"
+        s = f"{to_string(t.left, parent=t)} → {to_string(t.right, parent=t)}"
+        if _type_priority(parent) <= _type_priority(t):
+            s = f"({s})"
+        return s
     elif isinstance(t, Fals):
         return "False"
     else:
