fe25e069468b9f4b44afd48b9538afe813300c1b
khanh <khanh.nguyen.contact@gmail.com>
2026-05-04 16:27:14 +0800

    auto_commit

--- PropLogicKernel/Serialize.lean
+++ PropLogicKernel/Serialize.lean
@@ -1,17 +1,29 @@
+
 import PropLogicKernel.Kernel
 import PropLogicKernel.Printer
 
 namespace PropLogicKernel.Serialize
 
-def toStringPropInGoal [Map α Nat P] (g: G α): String × List (Nat × String) :=
-  let goal := s!"⊢ {g.goal}"
-  let hyp := (Map.iter g.hyp).map (λ (n, p) =>
-    (n, s!"{p}")
-  : Nat × P → Nat × String)
+def eraseAdjacentDups [BEq α] (l: List α): List α :=
+  match l with
+    | [] => l
+    | _ :: [] => l
+    | x1 :: x2 :: xs =>
+      if x1 == x2 then
+        eraseAdjacentDups (x2 :: xs)
+      else
+        x1 :: eraseAdjacentDups (x2 :: xs)
 
-  (goal, hyp)
 
+-- make goal into a string with unique hypotheses
+def serializeGoal [Map α Nat P] (g: G α): String :=
+  let (goal, hyp) := Printer.toStringPropInGoal g
+  let hyp: List String := (hyp.map (λ (_, p) => p))
+  let hyp := eraseAdjacentDups (hyp.mergeSort)
 
+  let pList: List String := goal :: hyp
+
+  String.intercalate "\n" pList
 
 
 
